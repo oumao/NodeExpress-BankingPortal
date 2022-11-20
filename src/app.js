3801ac3141694148346bf4c72b42d255e4efecfa
oumao <evansotis2015@gmail.com>
2022-11-20 22:19:12 +0300

    Fixed Static files

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const { allPass } = require('ramda')
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
-app.set('public', path.join(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, '/public')))
 
 
 app.get('/', (req, res)=>{
