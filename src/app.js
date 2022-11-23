8ff5652703eadf67ca1a796494d109a11ecd457a
oumao <evansotis2015@gmail.com>
2022-11-23 22:41:17 +0300

    Setting up Accounts

--- src/app.js
+++ src/app.js
@@ -1,19 +1,44 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const { allPass } = require('ramda')
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, '/public')))
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
 
+const accountData = fs.readFileSync(
+  path.join(__dirname, "/json/accounts.json"),
+  { encoding: "UTF8" }
+);
+const accounts = JSON.parse(accountData);
 
-app.get('/', (req, res)=>{
-    res.render('index', {title: 'Index' })
-})
+const userData = fs.readFileSync(path.join(__dirname, "/json/users.json"), {
+  encoding: "UTF8",
+});
+const users = JSON.parse(userData);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Account Summary", accounts: accounts });
+});
+
+app.get("/savings", (req, res) => {
+  res.render("account", { accounts: accounts.savings });
+});
+
+app.get("/checking", (req, res) => {
+  res.render("account", { accounts: accounts.checking });
+});
+
+app.get("/credit", (req, res) => {
+  res.render("account", { accounts: accounts.credit });
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: users[0] });
+});
 
 app.listen(3000, () => {
-    console.log("PS Project Running on port 3000")
-})
+  console.log("PS Project Running on port 3000");
+});
